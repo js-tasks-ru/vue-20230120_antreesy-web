93f0517250b7bbe2e47f899e6e1ce2f029c92036
Максим Сухарев <tris-z@mail.ru>
2023-01-20 19:46:32 +0100

    add basics/01 solution

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,11 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const app = createApp({
+  data() {
+    return { count: 5 };
+  },
+});
+
+app.mount('#app');
