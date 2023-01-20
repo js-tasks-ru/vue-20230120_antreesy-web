6abd0ea142cc257cf1b0c6448a75677b4f880ee4
Максим Сухарев <tris-z@mail.ru>
2023-01-20 20:00:41 +0100

    add basics/02 solution

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const app = createApp({
+  data() {
+    return {
+      inputFirst: 0,
+      inputSecond: 0,
+      operator: null,
+    };
+  },
+
+  computed: {
+    result() {
+      switch (this.operator) {
+        case 'sum': {
+          return this.inputFirst + this.inputSecond;
+        }
+        case 'subtract': {
+          return this.inputFirst - this.inputSecond;
+        }
+        case 'multiply': {
+          return this.inputFirst * this.inputSecond;
+        }
+        case 'divide': {
+          return this.inputFirst / this.inputSecond;
+        }
+        default:
+          return 'Pick an operator';
+      }
+    },
+  },
+
+  methods: {
+    changeOperator(event) {
+      this.operator = event.target.value;
+    },
+  },
+});
+
+app.mount('#app');
