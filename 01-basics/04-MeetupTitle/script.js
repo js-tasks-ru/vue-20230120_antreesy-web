80a7bf6ce037192ae162d480f934c4ba5fd1c8b9
Максим Сухарев <tris-z@mail.ru>
2023-01-22 15:26:42 +0100

    add basics/04 solution

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,20 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+
+const app = createApp({
+  data() {
+    return {
+      title: '',
+      idList: [1, 2, 3, 4, 5],
+      activeId: null,
+    };
+  },
+  watch: {
+    activeId() {
+      fetchMeetupById(this.activeId).then((res) => (this.title = res.title));
+    },
+  },
+});
+
+app.mount('#app');
