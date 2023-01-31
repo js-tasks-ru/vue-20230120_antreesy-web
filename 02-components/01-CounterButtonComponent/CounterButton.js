83be73b760c24d0e70141ac62ee3df1ca9dc6f49
Максим Сухарев <tris-z@mail.ru>
2023-01-31 20:08:30 +0100

    add task 02-01 / CounterButton

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,6 +4,18 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр и порождать событие
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
-  template: `<button type="button">1</button>`,
+  emits: ['update:count'],
+
+  template: `
+    <button type="button" @click="$emit('update:count', count + 1 )">
+      {{ count }}
+    </button>
+  `,
 });
